5f668979ffd952737578103ac726b319c6bfc291
Gregor <gregor-j@users.noreply.github.com>
2020-02-21 14:37:44 +0100

    add first version of interval class + test

---
+++ src/SapDateInterval.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * File src/SapDateInterval.php
+ *
+ * SAP time to interval conversions for times > 24h.
+ *
+ * @package datetime
+ * @author  Gregor J.
+ * @license MIT
+ */
+
+namespace phpsap\DateTime;
+
+use DateInterval;
+
+/**
+ * Class SapDateInterval
+ *
+ * Helps converting SAP time values to intervals for times greater than 24 hours.
+ *
+ * @package phpsap\DateTime
+ * @author  Gregor J.
+ * @license MIT
+ */
+class SapDateInterval extends DateInterval
+{
+    /**
+     * @var string SAP time format for DateInterval
+     */
+    const SAP_TIME = '%H%I%S';
+
+    /**
+     * Sets up a DateInterval from the relative parts of the string
+     * @param string $time
+     * @return \DateInterval
+     * @throws \Exception
+     * @link https://php.net/manual/en/dateinterval.createfromdatestring.php
+     */
+    public static function createFromDateString($time)
+    {
+        $matches = [];
+        if (preg_match('~^([\d]{2})([0-5][\d])([0-5][\d])$~', $time, $matches)) {
+            return new parent(sprintf(
+                'PT%sH%sM%sS',
+                $matches[1],
+                $matches[2],
+                $matches[3]
+            ));
+        }
+        return parent::createFromDateString($time);
+    }
+}
---
+++ tests/SapDateIntervalTest.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * File tests/SapDateIntervalTest.php
+ *
+ * Test SapDateInterval class.
+ *
+ * @package datetime
+ * @author  Gregor J.
+ * @license MIT
+ */
+
+namespace tests\phpsap\DateTime;
+
+use phpsap\DateTime\SapDateInterval;
+
+/**
+ * Class SapDateIntervalTest
+ *
+ * Unit tests for the SapDateTime class.
+ *
+ * @package tests\phpsap\DateTime
+ * @author  Gregor J.
+ * @license MIT
+ */
+class SapDateIntervalTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Data provider for valid time return values from SAP.
+     * @return array
+     */
+    public static function provideValidTimes()
+    {
+        return [
+            ['010203', '2020-01-31 00:00:00', '2020-01-31 01:02:03'],
+            ['263052', '2020-01-31 00:00:00', '2020-02-01 02:30:52'],
+        ];
+    }
+
+    /**
+     * Test converting valid times from SAP to an ISO date and time after 2020-01-31.
+     * @param string $sapTime
+     * @param string $startDate
+     * @param string $expected
+     * @throws \Exception
+     * @dataProvider provideValidTimes
+     */
+    public function testValidTimes($sapTime, $startDate, $expected)
+    {
+        $dateTime = new \DateTime($startDate);
+        $time = SapDateInterval::createFromDateString($sapTime);
+        $dateTime->add($time);
+        static::assertSame($expected, $dateTime->format('Y-m-d H:i:s'));
+    }
+}
